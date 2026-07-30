ff0d767ad6f1a4e9c36806f19fef9b1c95a6f366
Lokesh N Kuler <lokikuler@gmail.com>
2026-07-30 20:16:40 +0530

    project update

--- Color_switcher/script1.js
+++ Color_switcher/script1.js
@@ -1,9 +1,9 @@
-const buttons = document.querySelectorAll('.button')
+/*const buttons = document.querySelectorAll('.button')
 const body = document.querySelector("body")
 
 buttons.forEach(function(button)
 {
-    
+   
     button.addEventListener('click',function(e){
        if(e.target.id=='grey'){
         body.style.backgroundColor='grey'
@@ -18,5 +18,25 @@ buttons.forEach(function(button)
        if(e.target.id=='blue'){
         body.style.backgroundColor='blue'
        }
+    })
+});*/
+
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+buttons.forEach(function(button){
+    button.addEventListener('click' , function(e){
+        if(e.target.id == 'grey'){
+            body.style.backgroundColor='grey'
+        }
+        if(e.target.id == 'yellow'){
+            body.style.backgroundColor='yellow'
+        }
+        if(e.target.id == 'blue'){
+            body.style.backgroundColor='blue'
+        }
+        if(e.target.id == 'white'){
+            body.style.backgroundColor='white'
+        }
     })
 });
